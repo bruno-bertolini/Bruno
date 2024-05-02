6eb888c4d775d9c98b958042fe2a79258683d611
Bruno <126162420+bruno-bertolini@users.noreply.github.com>
2024-05-02 12:08:35 -0300

    condiçâo tempo final

--- main.js
+++ main.js
@@ -36,6 +36,13 @@ function calculaTempo(tempoObjetivo){
     minutos %=60;
     horas %=24;
 
+    if(tempoFinal >0){
+        return[dias,horas,minutos,segundos];
+}
+    else{
+        return[0,0,0,0];
+}
+
 
     return dias + " dias " + horas + " horas " + minutos +" minutos " + segundos + " segundos";
 }
